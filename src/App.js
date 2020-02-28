97fc7187b5b024fe81f79c06b34f10614186c1ee
Cícero Alvarenga <ciceroalvarenga2017@hotmail.com>
2020-02-28 16:22:51 -0300

    primera versao do projeto de receitas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const YOUR_APP_ID = 'a8c8ec15';
+  const YOUR_APP_KEY = 'f0c397c829b3200f3f2ceea5af80c689';
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+
+  const pegaReceita = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
+    );
+
+    const data = await response.json();
+    console.log('sou o data', data);
+    setRecipes(data.hits);
+    console.log(response);
+    console.log('eoque =>', data.hits);
+  };
+
+  useEffect(() => {
+    pegaReceita();
+  }, [query]);
+
+  const updateSearch = event => {
+    setSearch(event.target.value);
+  };
+
+  const getSearch = event => {
+    event.preventDefault();
+    setQuery(search);
+    setSearch('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          type="text"
+          className="search-bar"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
